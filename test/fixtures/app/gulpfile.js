2adca8f85d51af8ad8ad02537a4dccd08fd891a0
Mykhailo Aleksiuk <mykhailo.aleksiuk@yola.com>
2023-06-19 20:08:56 +0300

    Update dependencies to support node v14.

--- test/fixtures/app/gulpfile.js
+++ test/fixtures/app/gulpfile.js
@@ -11,13 +11,13 @@ gulp.task('clean', function(done) {
   del(['build'], { cwd: __dirname, force: true }, done);
 });
 
-gulp.task('build', ['clean'], function(){
+gulp.task('build', gulp.series('clean', function(){
   return gulp.src(['src/**'], {
       cwd: __dirname,
       base: path.join(__dirname, 'src')
     })
     .pipe(gulp.dest('build', { cwd: __dirname }));
-});
+}));
 var localePath = path.join(__dirname, 'locale');
 var installDepLocalePath = path.join(__dirname, 'installed_deps/locale');
 var options = {
@@ -26,24 +26,27 @@ var options = {
   jsonKeys: ['title']
 };
 
-gulp.task('translate', ['build'], function(){
+gulp.task('translate', gulp.series('build', function(){
   return gulp.src(['build'], { cwd: __dirname })
     .pipe(statici18n({localeDirs: [localePath]}));
-});
+}));
 
-gulp.task('translate-with-deps', ['build'], function(){
+gulp.task('translate-with-deps', gulp.series('build', function(){
   return gulp.src(['build'], { cwd: __dirname })
     .pipe(statici18n(options));
-});
+}));
 
-gulp.task('translate-with-null-default-lang', ['build'], function(){
+gulp.task('translate-with-null-default-lang', gulp.series('build', function(){
   var opts = clone(options);
   opts.defaultLang = null;
   return gulp.src(['build'], { cwd: __dirname })
     .pipe(statici18n(opts));
-});
+}));
 
-gulp.task('default', ['translate-with-deps']);
-gulp.task('single', ['translate']);
-
+gulp.task('default', gulp.series('translate-with-deps'));
+gulp.task('single', gulp.series('translate'));
+gulp.start = function (task, cb) {
+  return gulp.series(task)(cb);
+};
 module.exports = gulp;
+
